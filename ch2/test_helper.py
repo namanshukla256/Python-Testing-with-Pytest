6a558d412d83412ee2298948b6a752b284d79a8d
Naman Shukla <namanshukla256@gmail.com>
2025-09-22 13:40:11 +0530

    assert helper function

--- ch2/test_helper.py
+++ ch2/test_helper.py
@@ -1,2 +1,19 @@
 from cards import Card
 import pytest
+
+# Helper function to assert that two Card objects are identical (Assertion helper function)
+def assert_identical(c1: Card, c2: Card):
+    __tracebackhide__ = True # Hide this function from tracebacks
+    assert c1 == c2
+    if c1.id != c2.id:
+        pytest.fail(f"IDs differ: {c1.id} != {c2.id}")
+
+def test_identical():
+    c1 = Card("foo", id=123)
+    c2 = Card("foo", id=123)
+    assert_identical(c1, c2) # This should pass
+
+def test_not_identical():
+    c1 = Card("foo", id=123)
+    c2 = Card("foo", id=456)
+    assert_identical(c1, c2) # This should fail with a clear message
